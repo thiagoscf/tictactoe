3aedbdfd4132564539fca78c2a37b1cef51ba12f
Thiago Felix <thiago@uken.com>
2019-09-28 15:56:03 -0400

    Added extension methods

---
+++ Assets/Scripts/ExtensionMethods/GameObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class GameObjectExtensions {
+  public static Coroutine Schedule(this MonoBehaviour mono, float time, UnityAction action) {
+    if (Mathf.Approximately(time, 0.0f) || time < 0) {
+      action?.Invoke();
+      return null;
+    }
+
+    return mono.StartCoroutine(Wait(time, action));
+  }
+
+  private static IEnumerator Wait(float seconds, UnityAction actionToPerform) {
+    yield return new WaitForSeconds(seconds);
+    actionToPerform?.Invoke();
+  }
+}
