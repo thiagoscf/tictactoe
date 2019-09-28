b0b1c91a1e76366d2ce4e661bca3bb4bd03dd761
Thiago Felix <thiago@uken.com>
2019-09-28 15:56:31 -0400

    Added view scripts

---
+++ Assets/Scripts/View/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+  [SerializeField] private Text resultText;
+  [SerializeField] private Button playAgainButton;
+  [SerializeField] private Button quitButton;
+
+  public void Initialize(UnityAction playAgainAction, UnityAction quitAction) {
+    this.playAgainButton.SetAction(playAgainAction);
+    this.quitButton.SetAction(quitAction);
+
+    this.gameObject.SetActive(false);
+  }
+
+  public void Show(GameLogic.Result result) {
+    switch (result) {
+      case GameLogic.Result.HumanWon:
+        this.resultText.text = "You won!";
+        break;
+      case GameLogic.Result.AiWon:
+        this.resultText.text = "You lost!";
+        break;
+      case GameLogic.Result.Draw:
+        this.resultText.text = "Draw!";
+        break;
+    }
+
+    this.gameObject.SetActive(true);
+  }
+}
---
+++ Assets/Scripts/View/Grid.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Grid : MonoBehaviour {
+  private const int ButtonCount = 9;
+  private const float AiTurnDelay = 1;
+
+  [SerializeField] private Transform buttonContainer;
+  [SerializeField] private GridButton buttonPrefab;
+  [SerializeField] private Text turnInfo;
+
+  private GridButton[] buttons;
+  private UnityAction<int, UnityAction> onButtonClickAction;
+
+  public void Initialize(UnityAction<int, UnityAction> onButtonClickAction) {
+    this.onButtonClickAction = onButtonClickAction;
+    this.buttons = new GridButton[ButtonCount];
+
+    for (int i = 0; i < this.buttons.Length; i++) {
+      this.buttons[i] = Instantiate(this.buttonPrefab, parent: this.buttonContainer);
+      this.buttons[i].Initialize(i, this.PlayHumanTurn);
+    }
+  }
+
+  public void PrepareForGameOver() {
+    this.SetInteractable(false);
+    this.turnInfo.text = string.Empty;
+  }
+
+  public void PrepareForAiTurn() {
+    this.SetInteractable(false);
+    this.turnInfo.text = "AI's turn";
+  }
+
+  public void PrepareForHumanTurn() {
+    this.SetInteractable(true);
+    this.turnInfo.text = "Human's turn";
+  }
+
+  public void PlayAiTurn(int buttonIndex, UnityAction onComplete) {
+    this.Schedule(
+      AiTurnDelay,
+      () => {
+        this.buttons[buttonIndex].SetText("O");
+        this.SetInteractable(true);
+        onComplete?.Invoke();
+      });
+  }
+
+  private void PlayHumanTurn(int buttonIndex) {
+    this.onButtonClickAction?.Invoke(buttonIndex, () => {
+      this.SetInteractable(false);
+      this.buttons[buttonIndex].SetText("X");
+    });
+  }
+
+  private void SetInteractable(bool interactable) {
+    foreach (GridButton gridButton in this.buttons) {
+      gridButton.interactable = interactable;
+    }
+  }
+}
